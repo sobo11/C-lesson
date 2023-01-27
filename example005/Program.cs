c13203c8aa854d76b4701ae21aa1ce588f46052e
Boris <solovev_boris_2006@mail.ru>
2023-01-27 21:42:00 +0300

    error

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Writ("Введите имя пльзователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine(" это Маша");
+}
+else
+{
+    Console.Write("Привет ,");
+    Console.WriteLine(username);
+}
