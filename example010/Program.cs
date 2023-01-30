10dad3cdd6e2047b1d4093d9b50c1b36df8405dc
Boris <solovev_boris_2006@mail.ru>
2023-01-30 20:55:10 +0300

    retheyheytq

---
+++ example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {33, 43, 45, 65, 32, 13, 43, 12};
+
+
+int n = array .Length;
+int find = 43;
+
+int index = 0;
+
+while (index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
