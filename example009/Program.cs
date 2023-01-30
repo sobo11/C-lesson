84da54d7a06be4d7f3be9891dd924a173cd4df6b
Boris <solovev_boris_2006@mail.ru>
2023-01-30 20:44:45 +0300

    esfvgstsrdt

---
+++ example009/Program.cs
@@ -0,0 +1,43 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+
+
+int[] array = {33, 43, 45, 65, 32, 13, 12};
+
+array[0] = 12;
+Console.WriteLine(array[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // int max1 = Max(a1, b1, c1);
+// // int max2 = Max(a2, b2, c2);
+// // int max3 = Max(a3, b3, c3);
+// // int max = Max(max1, max2, max3);
+// int max = Max(Max(a1, b1, c1), 
+//     Max(a2, b2, c2), 
+//     Max(a3, b3, c3));
+
