1f239ac2fb3ed5ded3635533c42cf88bc749c1ae
Boris <solovev_boris_2006@mail.ru>
2023-02-02 21:07:40 +0300

    initial commit

---
+++ example012/Program.cs
@@ -0,0 +1,28 @@
+﻿//вид 1
+void Method1()
+{
+    Console.WriteLine("1");
+}
+Method1(); //вызывает метод
+
+//вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("текст");
+
+
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg:"q",count:6);
+
+
