d194587184bd6e40184bba45b1ea76c39fe51800
Boris <solovev_boris_2006@mail.ru>
2023-02-02 21:19:54 +0300

    012,1

--- example012/Program.cs
+++ example012/Program.cs
@@ -26,3 +26,32 @@ void Method21(string msg, int count)
 Method21(msg:"q",count:6);
 
 
+// вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+
+
+
+//вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; //String.Empty - пустая строка
+    while(i<count)
+    {
+        result = result + text;
+        i ++;
+    }
+    return result;
+}
+
+string res = Method4(10, "rty");
+Console.WriteLine(res);
+
